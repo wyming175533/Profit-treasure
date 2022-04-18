e9f44d6b9a3da5d0cfa82aa5c5bc51182b5725f4
wangyiming <1755337368@qq.com>
2022-04-18 10:23:15 +0800

    定时任务服务

---
+++ micro-timedtask/src/main/java/com/bjpowernode/microtimedtask/MicroTimedtaskApplication.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.microtimedtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTimedtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTimedtaskApplication.class, args);
+	}
+
+}
---
+++ micro-timedtask/src/test/java/com/bjpowernode/microtimedtask/MicroTimedtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.microtimedtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTimedtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
